9b339df6cbe8030019f873325345ed648adb6a68
Patrick Villanueva <villanuv@yahoo.com>
2018-04-26 18:22:47 -0500

    new index page with youtube search

---
+++ js/search.js
@@ -0,0 +1,28 @@
+function keyWordsearch() {
+  gapi.client.setApiKey('AIzaSyD89Rr5p7H20AI-YqPIDS5AxTEWNwWwDd4');
+  gapi.client.load('youtube', 'v3', function() {
+    makeRequest();
+  });
+}
+
+function makeRequest() {
+  var q = $('#query').val();
+  var request = gapi.client.youtube.search.list({
+    q: q,
+    maxResults: 10,
+    type: 'video',
+    part: 'snippet'                        
+  });
+
+  request.execute(function(response) {
+    var str = JSON.stringify(response.result['items']);
+    var data = JSON.parse(str);
+    $('#search-results').html('');
+
+    for(i=0; i<data.length; i++) {
+      var vidId = data[i]['id']['videoId'];
+      var vidTitle = data[i]['snippet']['title'];
+      $('#search-results').append('<a href="https://www.youtube.com/watch?v=' + vidId + '" target="_blank">' + vidTitle + '</a><br>');
+    }
+  });
+}
