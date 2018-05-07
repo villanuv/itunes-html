54518c0132c170ea0076faa71225d610b899e403
Patrick Villanueva <villanuv@yahoo.com>
2018-05-06 22:04:19 -0500

    updated live search display, cleanup

---
+++ js/app.js
@@ -0,0 +1,272 @@
+var tag = document.createElement('script');
+
+tag.src = "https://www.youtube.com/iframe_api";
+var firstScriptTag = document.getElementsByTagName('script')[0];
+firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+
+var player;
+function onYouTubeIframeAPIReady() {
+  player = new YT.Player('player', {
+    height: '134',
+    width: '221',
+    playerVars: { 
+      'fs': 0, 
+      'showinfo': 0,
+      'controls': 0,
+      'color': 'red',
+      'modestbranding': 1,
+      'rel': 0,
+      'start': 1,
+      'playlist': ['dQw4w9WgXcQ']
+    },
+    events: {
+      'onReady': onPlayerReady,
+      'onStateChange': onPlayerStateChange
+    }
+  });
+}
+
+function onPlayerReady() {
+  $('#pause').hide();
+  $('#play').show();
+}
+
+function onPlayerStateChange(event) {
+  if(event.data == 0) {
+    // togglePlay();
+  }
+}
+
+$('#progress').css('width', 0+'%');
+
+var playlistArray = [];
+
+function getTimes(){
+  var position = player.getCurrentTime();
+  var duration = player.getDuration();
+  $('#progress').css('width', Math.floor(100*position/duration)+'%');
+
+  var timeString = (position/duration) * duration;
+  var timeLeft = duration - Math.floor(position);
+  var timeStrMin = Math.floor(timeString/60);
+  var timeLeftMin = Math.floor(timeLeft/60);
+  if (timeString % 60 < 10){
+    var timeStrSec = '0' + Math.floor(timeString % 60);
+  } else {
+    var timeStrSec = Math.floor(timeString % 60);
+  }
+  if (timeLeft % 60 < 10){
+    var timeLeftSec = '0' + Math.floor(timeLeft % 60);
+  } else {
+    var timeLeftSec = Math.floor(timeLeft % 60);
+  }
+  var timeText1 = timeStrMin + ':' + timeStrSec;
+  var timeText2 = '-' + timeLeftMin + ':' + timeLeftSec;
+  if (timeText1 == "NaN:NaN") {
+    timeText1 = "0:00";
+  }
+  $('#time1').text(timeText1);
+  $('#time2').text(timeText2);   
+  setTimeout(getTimes, 500);
+}
+
+$('#previous').click(function() { 
+  // $('#api').rdio().previous(); 
+});
+
+$('#play').click(function() { 
+  player.playVideo();
+  $('#play').hide();
+  $('#pause').show();
+});
+
+$('#pause').click(function() { 
+  player.pauseVideo();
+  $('#pause').hide();
+  $('#play').show();
+});
+
+$('#next').click(function() {
+  // $('#api').rdio().next(); 
+});
+
+$(".container").draggable({
+  handle: ".topBar",
+  delay: 300
+});
+
+function setVolume(volume) {
+  player.setVolume(volume);
+};
+
+$('#slider').slider({
+  min: 0,
+  max: 82,
+  value: 65,
+  slide: function(event, ui) {
+    setVolume(Math.floor(ui.value/82*100));
+  }
+});
+
+$('#scrobble').slider({
+  orientation: "horizontal",
+  range: "min",
+  max: 100,
+  value: 20
+});
+
+var repeatImg = ['images/repeat-off.jpg', 'images/repeat-one.jpg', 'images/repeat-all.jpg'];
+var clickImg = 0;
+
+function setRepeatImg(){
+  $("#repeatContainer img").click(changeImage);
+}
+
+function changeImage(){
+  if (clickImg == 2){
+    clickImg = 0;
+  } else {
+    clickImg++;
+  }
+  img = repeatImg[clickImg];
+  $("#repeatContainer img").attr("src", img);
+  // $("#api").rdio().setRepeat(clickImg);
+}
+
+setRepeatImg();
+
+var toggle = 0;
+
+$('#shuffleContainer img').click(function(){
+  if (toggle == 0){
+    toggle = 1;
+  } else {
+    toggle = 0;
+  }
+  $('#shuffleContainer img').toggle();
+  // $("#api").rdio().setShuffle(toggle);
+});
+
+function getDateString(){
+  var d = new Date();
+  var weekday = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+  var dateString = weekday[d.getDay()];
+  var timeHr = d.getHours();
+  var timeMin = d.getMinutes();
+  if (timeHr < 12) {
+    var ampm = 'AM';
+  } else {
+    var ampm = 'PM';
+  }
+  if (timeHr == 0) {
+    timeHr = 12;
+  } else if (timeHr >= 13) {
+    timeHr = timeHr - 12;
+  }
+  if (timeMin < 10) {
+    timeMin = '0' + timeMin;
+  }
+  var timeString = timeHr + ':' + timeMin;
+
+  dateString = dateString + ' ' + timeString + ' ' + ampm;
+  $('#date').text(dateString);
+  setTimeout(getDateString, 5000);
+};
+
+getDateString();
+
+
+var App = angular.module('RdioApp', ['ngDragDrop']);
+
+App.controller('TrackController', function($scope, $http){
+
+  $scope.playlist = [];
+
+  $scope.searchResults = [
+    {id: "dQw4w9WgXcQ", name: "Rick Astley - Never Gonna Give You Up", thumb: "https://i.ytimg.com/vi/dQw4w9WgXcQ/default.jpg"},
+    {id: "8UVNT4wvIGY", name: "Gotye - Somebody That I Used To Know", thumb: "https://i.ytimg.com/vi/8UVNT4wvIGY/default.jpg"},
+    {id: "sENM2wA_FTg", name: "Imagine Dragons - It's Time", thumb: "https://i.ytimg.com/vi/sENM2wA_FTg/default.jpg"},
+    {id: "C-dvTjK_07c", name: "Usher - DJ Got Us Fallin' in Love", thumb: "https://i.ytimg.com/vi/C-dvTjK_07c/default.jpg"},
+    {id: "U5rLz5AZBIA", name: "Timbaland - The Way I Are", thumb: "https://i.ytimg.com/vi/U5rLz5AZBIA/default.jpg"},
+    {id: "oG08ukJPtR8", name: "Michael Jackson & Justin Timberlake - Love Never Felt so Good", thumb: "https://i.ytimg.com/vi/oG08ukJPtR8/default.jpg"},
+    {id: "OPf0YbXqDm0", name: "Mark Ronson feat. Bruno Mars - Uptown Funk", thumb: "https://i.ytimg.com/vi/OPf0YbXqDm0/default.jpg"},
+    {id: "rog8ou-ZepE", name: "Vanilla Ice - Ice Ice Baby", thumb: "https://i.ytimg.com/vi/rog8ou-ZepE/default.jpg"},
+    {id: "9bZkp7q19f0", name: "Psy - Gangnam Style (강남스타일)", thumb: "https://i.ytimg.com/vi/9bZkp7q19f0/default.jpg"} 
+  ];
+
+  $scope.submit = function(){
+    gapi.client.setApiKey('AIzaSyD89Rr5p7H20AI-YqPIDS5AxTEWNwWwDd4');
+    gapi.client.load('youtube', 'v3', function() {
+
+      var q = $('#searchField').val();
+      gtag('config', 'UA-118583968-1', {'page_path': "/?q='"+q+"'"});
+
+      var request = gapi.client.youtube.search.list({
+        q: q,
+        maxResults: 20,
+        type: 'video',
+        videoCategoryId: 10,
+        part: 'snippet',
+        videoEmbeddable: true                     
+      });
+
+      var filteredTracks = [];
+
+      request.execute(function(response) {
+        var str = JSON.stringify(response.result['items']);
+        var data = JSON.parse(str);
+
+        for(i=0; i<data.length; i++) {
+          var vidId = data[i]['id']['videoId'];
+          var vidTitle = data[i]['snippet']['title'];
+          var vidThumb = data[i]['snippet']['thumbnails']['default']['url'];
+          var vidTitle = data[i]['snippet']['title'];
+          filteredTracks.push({id: vidId, name: vidTitle, thumb: vidThumb});
+        }
+      });
+
+      setTimeout(function(){
+        $scope.$apply(function() {
+          $scope.searchResults = filteredTracks;
+        });
+      }, 1000);
+
+    });
+
+
+
+  }
+
+  $scope.setMaster = function(song){
+    $scope.selected = song;
+  }
+
+  $scope.isSelected = function(song){
+    return $scope.selected === song;
+  }
+
+  $scope.addThis = function(){
+    var newSong = $scope.playlist[$scope.playlist.length-1].id;
+    playlistArray.unshift(newSong);
+  }
+
+  $scope.dblClickedPlay = function(){
+    $('.mainText').css('background', 'none');
+    $('#trackName').html($scope.selected.name);
+    $('#coverArt').attr('style', "background:url('" + $scope.selected.thumb + "');");
+    player.loadVideoById($scope.selected.id);
+    $('#play').hide();
+    $('#pause').show();
+    getTimes();
+  };  
+
+  $scope.dblClicked = function(){
+    $('.mainText').css('background', 'none');
+    $('#trackName').html($scope.selected.name);
+    $('#coverArt').attr('style', "background:url('" + $scope.selected.thumb + "');");
+    player.loadVideoById($scope.selected.id);
+    $('#play').hide();
+    $('#pause').show();
+    getTimes();
+  };
+});
