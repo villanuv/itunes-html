79488d0ad2fcda85567c6e609e701f16fd7e22f7
Patrick Villanueva <villanuv@yahoo.com>
2018-05-13 00:00:11 -0500

    edited analytics, reconfigured files

--- js/app.js
+++ js/app.js
@@ -32,16 +32,66 @@ function onPlayerReady() {
 }
 
 function onPlayerStateChange(event) {
+  if(event.data == 1 && window.currentPlaylist != undefined) {
+    updateTrackData();
+  }
+  if(event.data == 1 && window.currentPlaylist == undefined) {
+    updateOneTrackData();
+  }
   if(event.data == 0) {
-    // togglePlay();
+    $('#pause').toggle();
+    $('#play').toggle();
   }
 }
 
+$(document).keydown(function(e) {
+  switch(event.altKey && e.which) {
+    case 32:
+      $('#pause').toggle();
+      $('#play').toggle();
+      if (player.getPlayerState() == 2 || player.getPlayerState() == 5) {
+        player.playVideo();
+      }
+      if (player.getPlayerState() == 1) {
+        player.pauseVideo();
+      }
+      break;
+    case 37:
+      player.seekTo(player.getCurrentTime()-3);
+      break;
+    case 39:
+      player.seekTo(player.getCurrentTime()+3);
+      break;
+    case 38:
+      player.setVolume(player.getVolume()+3);
+      var newVolume = Math.floor(player.getVolume()/100*82);
+      $('#slider').slider({value: newVolume});
+      break;
+    case 40:
+      player.setVolume(player.getVolume()-3);
+      var newVolume = Math.floor(player.getVolume()/100*82);
+      $('#slider').slider({value: newVolume});
+      break;
+    case 219:
+      if(window.currentPlaylist != undefined){
+        player.previousVideo();
+      }
+      break;
+    case 221:
+      if(window.currentPlaylist != undefined){
+        player.nextVideo();
+      }
+      break;
+    default: 
+      return;
+  }
+  e.preventDefault();
+});
+
+
 $('#progress').css('width', 0+'%');
 
-var playlistArray = [];
-
-function getTimes(){
+function getTimes() {
   var position = player.getCurrentTime();
   var duration = player.getDuration();
   $('#progress').css('width', Math.floor(100*position/duration)+'%');
@@ -70,8 +120,43 @@ function getTimes(){
   setTimeout(getTimes, 500);
 }
 
+
+function updateTrackData() {
+  var playlistData = window.currentPlaylist['tracks'];
+  var playerCurrentIndex = player.getPlaylistIndex();
+  var nowPlayingObj = playlistData[playerCurrentIndex];
+  recentlyPlayedList['tracks'].push(nowPlayingObj);
+
+  $('.mainText').css('background', 'none');
+  $('#trackName').html(nowPlayingObj.title);
+  $('#artistAlbum').html(nowPlayingObj.channelTitle);
+  $('#coverArt').attr('style', "background:url('" + nowPlayingObj.thumb + "');");
+  $('#play').hide();
+  $('#pause').show();
+  gtag('config', 'UA-118583968-1', {'page_path': "/?playlist='" + window.currentPlaylist['name'] + "'&v='"+nowPlayingObj.title+"'"});
+  getTimes();
+}
+
+function updateOneTrackData() {
+  if($('#trackName').html() == ""){
+    var track = player.getVideoData();
+    $('.mainText').css('background', 'none');
+    $('#trackName').html(track['title']);
+    $('#artistAlbum').html(track['author']);
+    gtag('config', 'UA-118583968-1', {'page_path': "/?v='"+track['title']+"'"});
+    getTimes();
+  }
+}
+
+
 $('#previous').click(function() { 
-  // $('#api').rdio().previous(); 
+  var currentTime = player.getCurrentTime();
+  var playlistCheck = player.getPlaylist();
+  if(currentTime < 5 && playlistCheck != null){
+    player.previousVideo();
+  } else {
+    player.seekTo(0);
+  }
 });
 
 $('#play').click(function() { 
@@ -87,12 +172,35 @@ $('#pause').click(function() {
 });
 
 $('#next').click(function() {
-  // $('#api').rdio().next(); 
+  var playlistCheck = player.getPlaylist();
+  if(playlistCheck != null){
+    player.nextVideo();
+  } else {
+    player.seekTo(player.getCurrentTime() + 3); 
+  }
 });
 
 $(".container").draggable({
   handle: ".topBar",
   delay: 300
+});
+
+$(".about-popup").draggable({
+  handle: ".popup-top",
+  delay: 300
+});
+
+$(".stickies").draggable({
+  handle: ".handle",
+  delay: 300
+});
+
+// $(".stickies .handle").dblclick(function(){
+//   $(".stickies .main").toggle();
+// });
+
+$(".stickies img").click(function(){
+  $(".stickies img").closest(".stickies").hide();
 });
 
 function setVolume(volume) {
@@ -115,36 +223,32 @@ $('#scrobble').slider({
   value: 20
 });
 
-var repeatImg = ['images/repeat-off.jpg', 'images/repeat-one.jpg', 'images/repeat-all.jpg'];
+var repeatImg = ['images/repeat-off.jpg', 'images/repeat-all.jpg']; //'images/repeat-one.jpg'
 var clickImg = 0;
+var loop = false;
 
 function setRepeatImg(){
   $("#repeatContainer img").click(changeImage);
 }
 
 function changeImage(){
-  if (clickImg == 2){
+  if (clickImg == 1){
     clickImg = 0;
   } else {
-    clickImg++;
+    clickImg = 1;
   }
   img = repeatImg[clickImg];
   $("#repeatContainer img").attr("src", img);
-  // $("#api").rdio().setRepeat(clickImg);
+  player.setLoop(!loop);
 }
 
 setRepeatImg();
 
-var toggle = 0;
+var shuffle = false;
 
 $('#shuffleContainer img').click(function(){
-  if (toggle == 0){
-    toggle = 1;
-  } else {
-    toggle = 0;
-  }
   $('#shuffleContainer img').toggle();
-  // $("#api").rdio().setShuffle(toggle);
+  player.setShuffle(!shuffle);
 });
 
 function getDateString(){
@@ -175,98 +279,212 @@ function getDateString(){
 
 getDateString();
 
+$('.app-name').click(function(){
+  $('.app-name').toggleClass("app-name-over");
+  $('.menu-popup').toggle();
+});
+
+$.fn.center = function() {
+  this.css("position","absolute");
+  this.css("top", ( jQuery(window).height() - this.height() ) / 2+jQuery(window).scrollTop() + "px");
+  this.css("left", ( jQuery(window).width() - this.width() ) / 2+jQuery(window).scrollLeft() + "px");
+  return this;
+};
+
+$('.menu-popup .text').click(function(){
+  $('.app-name').toggleClass("app-name-over");
+  $('.menu-popup').toggle();
+  $('.about-popup').center();
+  $('.about-popup').toggle();
+});
+
+$('.popup-top img:first-child')
+    .mouseover(function() { 
+        $(this).attr("src", "images/btn-red-over.gif");
+    })
+    .mouseout(function() {
+       $(this).attr("src", "images/btn-red.gif");
+    });
+
+
+$('.popup-top img:first-child').click(function(){
+  $('.about-popup').toggle();
+});
+
+
+
 
 var App = angular.module('RdioApp', ['ngDragDrop']);
 
 App.controller('TrackController', function($scope, $http){
 
-  $scope.playlist = [];
+  $scope.playlists = [recentlyPlayedList, iTunesHTMLPicks, wysPlaylist, ninetiesHousePlaylist, newEditionStoryPlaylist, cobrakaiPlaylist, abbaPlaylist];
 
-  $scope.searchResults = [
-    {id: "dQw4w9WgXcQ", name: "Rick Astley - Never Gonna Give You Up", thumb: "https://i.ytimg.com/vi/dQw4w9WgXcQ/default.jpg"},
-    {id: "8UVNT4wvIGY", name: "Gotye - Somebody That I Used To Know", thumb: "https://i.ytimg.com/vi/8UVNT4wvIGY/default.jpg"},
-    {id: "sENM2wA_FTg", name: "Imagine Dragons - It's Time", thumb: "https://i.ytimg.com/vi/sENM2wA_FTg/default.jpg"},
-    {id: "C-dvTjK_07c", name: "Usher - DJ Got Us Fallin' in Love", thumb: "https://i.ytimg.com/vi/C-dvTjK_07c/default.jpg"},
-    {id: "U5rLz5AZBIA", name: "Timbaland - The Way I Are", thumb: "https://i.ytimg.com/vi/U5rLz5AZBIA/default.jpg"},
-    {id: "oG08ukJPtR8", name: "Michael Jackson & Justin Timberlake - Love Never Felt so Good", thumb: "https://i.ytimg.com/vi/oG08ukJPtR8/default.jpg"},
-    {id: "OPf0YbXqDm0", name: "Mark Ronson feat. Bruno Mars - Uptown Funk", thumb: "https://i.ytimg.com/vi/OPf0YbXqDm0/default.jpg"},
-    {id: "rog8ou-ZepE", name: "Vanilla Ice - Ice Ice Baby", thumb: "https://i.ytimg.com/vi/rog8ou-ZepE/default.jpg"},
-    {id: "9bZkp7q19f0", name: "Psy - Gangnam Style (강남스타일)", thumb: "https://i.ytimg.com/vi/9bZkp7q19f0/default.jpg"} 
-  ];
+  $scope.searchResults = iTunesHTMLPicks['tracks'];
 
   $scope.submit = function(){
-    gapi.client.setApiKey('AIzaSyD89Rr5p7H20AI-YqPIDS5AxTEWNwWwDd4');
+    gapi.client.setApiKey("AIzaSyD89Rr5p7H20AI-YqPIDS5AxTEWNwWwDd4");
     gapi.client.load('youtube', 'v3', function() {
 
       var q = $('#searchField').val();
       gtag('config', 'UA-118583968-1', {'page_path': "/?q='"+q+"'"});
+      $scope.selectedPL = "";
 
       var request = gapi.client.youtube.search.list({
         q: q,
-        maxResults: 20,
+        maxResults: 1,
         type: 'video',
         videoCategoryId: 10,
         part: 'snippet',
-        videoEmbeddable: true                     
-      });
-
-      var filteredTracks = [];
-
-      request.execute(function(response) {
-        var str = JSON.stringify(response.result['items']);
-        var data = JSON.parse(str);
-
-        for(i=0; i<data.length; i++) {
-          var vidId = data[i]['id']['videoId'];
-          var vidTitle = data[i]['snippet']['title'];
-          var vidThumb = data[i]['snippet']['thumbnails']['default']['url'];
-          var vidTitle = data[i]['snippet']['title'];
-          filteredTracks.push({id: vidId, name: vidTitle, thumb: vidThumb});
+        videoEmbeddable: true   
+      }).then(function(response) {
+        if(response.result.pageInfo.totalResults != 0) {
+          return response.result.items[0].id.videoId; // just an id
         }
-      });
-
-      setTimeout(function(){
-        $scope.$apply(function() {
-          $scope.searchResults = filteredTracks;
+      }).then(function(vidId) {
+        return gapi.client.youtube.videos.list({
+          part: 'snippet,statistics,contentDetails',
+          id: vidId
         });
-      }, 1000);
+      }).then(function(response) { 
+        var returnObj = {};
+        console.log(response.result.items);
+        if(response.result.pageInfo.totalResults != 0) {
+          returnObj['id'] = response.result.items[0].id;
+          returnObj['title'] = response.result.items[0].snippet.title;
+          returnObj['channelTitle'] = response.result.items[0].snippet.channelTitle;
+          returnObj['thumb'] = response.result.items[0].snippet.thumbnails.default.url;
+          returnObj['publishedAt'] = response.result.items[0].snippet.publishedAt;
+          returnObj['duration'] = response.result.items[0].contentDetails.duration;
+          returnObj['viewCount'] = response.result.items[0].statistics.viewCount;
+          returnObj['likeCount'] = response.result.items[0].statistics.likeCount;
+          returnObj['dislikeCount'] = response.result.items[0].statistics.dislikeCount;
+        }
+        return returnObj;
+      }).then(function(returnObj) {
+        console.log(returnObj);
+        $scope.$apply(function() {
+          $scope.searchResults = [returnObj];
+        });
+      });
 
     });
-
-
-
-  }
+  };
 
   $scope.setMaster = function(song){
     $scope.selected = song;
-  }
+    $scope.selectedPL = "";
+  };
 
   $scope.isSelected = function(song){
     return $scope.selected === song;
-  }
+  };
 
-  $scope.addThis = function(){
-    var newSong = $scope.playlist[$scope.playlist.length-1].id;
-    playlistArray.unshift(newSong);
-  }
+  $scope.setMasterPL = function(playlist){
+    $scope.selectedPL = playlist;
+    $scope.searchResults = playlist['tracks'];
+  };
 
-  $scope.dblClickedPlay = function(){
-    $('.mainText').css('background', 'none');
-    $('#trackName').html($scope.selected.name);
-    $('#coverArt').attr('style', "background:url('" + $scope.selected.thumb + "');");
-    player.loadVideoById($scope.selected.id);
-    $('#play').hide();
-    $('#pause').show();
-    getTimes();
-  };  
+  $scope.isSelectedPL = function(playlist){
+    return $scope.selectedPL === playlist;
+  };
+
+
+  // $scope.addThis = function(){
+  //   var newSong = $scope.playlist[$scope.playlist.length-1].id;
+  //   playlistArray.unshift(newSong);
+
+// for new playlist
+// a = player.getPlaylist();
+// a.push('dQw4w9WgXcQ');
+// b = player.getPlaylistIndex();
+// player.pauseVideo();
+// c = player.getCurrentTime();
+// player.loadPlaylist(a);
+// player.playVideoAt(b);
+// player.seekTo(c);
+
+  // };
 
   $scope.dblClicked = function(){
     $('.mainText').css('background', 'none');
-    $('#trackName').html($scope.selected.name);
+    $('#trackName').html($scope.selected.title);
+    $('#artistAlbum').html($scope.selected.channelTitle);
     $('#coverArt').attr('style', "background:url('" + $scope.selected.thumb + "');");
     player.loadVideoById($scope.selected.id);
+    recentlyPlayedList['tracks'].push($scope.selected);
     $('#play').hide();
     $('#pause').show();
+    gtag('config', 'UA-118583968-1', {'page_path': "/?v='"+$scope.selected.title+"'"});
     getTimes();
   };
+
+  $scope.dblClickedPL = function(playlist){
+    window.currentPlaylist = playlist;
+    var allTrackIDs = [];
+    var tracks = playlist['tracks'];
+    for(i=0;i<tracks.length;i++){
+      allTrackIDs.push(tracks[i]['id']);
+    }
+    player.loadPlaylist(allTrackIDs);
+    gtag('config', 'UA-118583968-1', {'page_path': "/?playlist='"+playlist['name']+"'"});
+  };
+
+});
+
+
+App.filter('convertYTDuration', function() {
+
+  return function(string) {
+    if (string != undefined) {
+      var tString = string.replace('H', ':').replace('M', ':').replace('PT', '').replace('S', '');
+      var tArray = tString.split(":");
+
+        if(tArray.length == 1) {
+          tArray.unshift("0");
+        }
+
+        if(tArray.length > 1) {
+          var minutes = tArray[tArray.length-2];
+          if(minutes.length == 1 && tArray.length == 3){
+            tArray[tArray.length-2] = '0' + minutes;
+          }
+          var seconds = tArray[tArray.length-1];
+          if(seconds.length == 1){
+            tArray.pop();
+            tArray.push('0' + seconds);
+          }
+          if(seconds.length == 0){
+            tArray.pop();
+            tArray.push('00');
+          }
+        }
+
+      return tArray.join(":");
+    } else {
+      return '';
+    }
+  }
+
+});
+
+
+App.filter('convertYTDate', function() {
+
+  return function(string) {
+    if (string != undefined) {
+      var ytArr = string.split("T")[0].split("-");
+      ytArr.push(ytArr.shift());
+      if(ytArr[0][0] == '0'){
+        ytArr[0] = ytArr[0][1];
+      }
+      if(ytArr[1][0] == '0'){
+        ytArr[1] = ytArr[1][1];
+      }
+      return ytArr.join("/");
+    } else {
+      return '';
+    }
+  }
+
 });
